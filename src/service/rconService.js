38361f67c4b01d11b8fe3bc1aeba4b02986ac1b5
James Wobser <james.wobser@gmail.com>
2022-11-22 19:19:52 -0500

    Add Tests
    Added tests for EssX version of the query commands. However,
    env vars cannot be changed, so only one test or the other can
    execute, with the env var hard-coded in.

--- src/service/rconService.js
+++ src/service/rconService.js
@@ -2,42 +2,47 @@ const rconServiceErrorEnum = require('../enums/rconServiceErrorEnum.js')
 const logger = require('../util/logger.js')
 
 const SERVER_RCON_PASS = process.env.SERVER_RCON_PASS
+// ESSENTIALS_X = false /* Manual override for testing */
 const ESSENTIALS_X = process.env.ESSENTIALS_X
 
 async function queryXpLevels(rconClient, userId) {
-    if (ESSENTIALS_X){
-        const cmdString = `xp show ${userId}`
+    cmdString = ''
+    if (ESSENTIALS_X == true){
+        cmdString = `xp show ${userId}`
     } else {
-        const cmdString = `xp query ${userId} levels`
+        cmdString = `xp query ${userId} levels`
     }
 
     await rconClient.authenticate(SERVER_RCON_PASS)
     const serverResp = await rconClient.execute(cmdString)
     rconClient.disconnect()
 
-    if (ESSENTIALS_X){
-        const levels = await parseEssXQueryLevels(serverResp)
+    levels = 0
+    if (ESSENTIALS_X == true){
+        levels = await parseEssXQueryLevels(serverResp)
     } else {
-        const levels = await parseQueryResp(serverResp)
+        levels = await parseQueryResp(serverResp)
     }
     logger.log(`[rconService] User ${userId} has ${levels} levels`)
     return levels
 }
 
 async function queryXpPoints(rconClient, userId) {
-    if (ESSENTIALS_X){
-        const cmdString = `xp show ${userId}`
+    cmdString = ''
+    if (ESSENTIALS_X == true){
+        cmdString = `xp show ${userId}`
     } else {
-        const cmdString = `xp query ${userId} points`
+        cmdString = `xp query ${userId} points`
     }
     await rconClient.authenticate(SERVER_RCON_PASS)
     const serverResp = await rconClient.execute(cmdString)
     rconClient.disconnect()
 
-    if (ESSENTIALS_X){
-        const points = await parseEssXQueryPoints(serverResp)
+    points = 0
+    if (ESSENTIALS_X == true){
+        points = await parseEssXQueryPoints(serverResp)
     } else {
-        const points = await parseQueryResp(serverResp)
+        points = await parseQueryResp(serverResp)
     }
     logger.log(`[rconService] User ${userId} has ${points} points`)
     return points
@@ -79,7 +84,7 @@ async function parseEssXQueryLevels(serverResponse) {
     const regexExp = /(?:level )([0-9]+)/
 
     if (regexExp.test(serverResponse)) {
-        return parseInt(serverResponse.match(regexExp)[0])
+        return parseInt(serverResponse.match(regexExp)[1])
     } else {
         logger.log('[rconService] Unexpected server response: ' + serverResponse)
         throw Error(rconServiceErrorEnum.RCON_FAILED)
@@ -94,7 +99,7 @@ async function parseEssXQueryPoints(serverResponse) {
     const regexExp = /(?:has )([0-9]+)/
 
     if (regexExp.test(serverResponse)) {
-        return parseInt(serverResponse.match(regexExp)[0])
+        return parseInt(serverResponse.match(regexExp)[1])
     } else {
         logger.log('[rconService] Unexpected server response: ' + serverResponse)
         throw Error(rconServiceErrorEnum.RCON_FAILED)
--- test/service/rconServiceTest.js
+++ test/service/rconServiceTest.js
@@ -2,12 +2,14 @@ const rconService = require('../../src/service/rconService.js')
 const rconServiceErrorEnum = require('../../src/enums/rconServiceErrorEnum.js')
 const expect = require('chai').expect
 const sinon = require('sinon')
+const process = require('process')
 
 const USER_ID = 'player'
 const XP_POINTS = 100
 const RESP_QUERY_LEVELS = 'player has 10 experience levels'
 const RESP_QUERY_POINTS = 'player has 10 experience points'
 const RESP_UNEXPECTED = 'some crap'
+const RESP_ESSX = 'player has 117 exp (level 8) and needs 18 more exp to level up.'
 
 describe('rconService: addXpPoints:', function() {
     describe('When no player is online', function() {
@@ -116,4 +118,31 @@ describe('rconService: queryXpLevels:', function() {
             expect(rconServiceResp).to.be.equal(10)
         })
     })
+})
+
+describe('rconService: queryEssX____:', function() {
+    process.env.ESSENTIALS_X = true
+    describe('When EssentialsX player levels are returned', function() {
+        it('Returns player levels count', async function() {
+            const rconClientMock = {
+                authenticate: sinon.stub().returns(),
+                execute: sinon.stub().returns(RESP_ESSX),
+                disconnect: sinon.stub().returns()
+            }
+            const rconServiceResp = await rconService.queryXpLevels(rconClientMock, USER_ID)
+            expect(rconServiceResp).to.be.equal(8)
+        })
+    })
+    describe('When EssentialsX player points are returned', function() {
+        it('Returns player points count', async function() {
+            const rconClientMock = {
+                authenticate: sinon.stub().returns(),
+                execute: sinon.stub().returns(RESP_ESSX),
+                disconnect: sinon.stub().returns()
+            }
+            const rconServiceResp = await rconService.queryXpPoints(rconClientMock, USER_ID)
+            expect(rconServiceResp).to.be.equal(117)
+        })
+    })
+
 })
